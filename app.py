301455286e8242806a205c1000668af8f5bedb63
Adarsh Kumar <adarshkr357@gmail.com>
2025-08-06 14:25:54 +0530

    PORT fix.

--- app.py
+++ app.py
@@ -93,5 +93,6 @@ def currency_api():
         return jsonify({'success': False, 'message': str(e)}), 500
 
 
-if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host="0.0.0.0", port=port)
